1439f69d502c6c82f063cadefbcf0ac59856824b
Ajay Singh Rathour <96001620+Ajaysinghrathour17@users.noreply.github.com>
2024-03-01 16:47:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss';
-import {FaBackspace} from 'react-icons/fa'
+// import {FaBackspace} from 'react-icons/fa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
